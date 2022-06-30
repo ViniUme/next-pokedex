aaf5cac76b6f0c33238e338726a2bc5720a6e746
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-30 15:22:08 -0300

    Using tag Image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,10 @@
 import styles from '../styles/navbar.module.scss';
+import Image from 'next/image';
 
 export default function Navbar(){
     return(
         <div className={styles.navbar}>
-            <img src="/img/Pokemon-Logo.png" className={styles.logo} />
+            <Image src='/img/Pokemon-Logo.png' alt="" width={280} height={130}/>
         </div>
     )
 }
