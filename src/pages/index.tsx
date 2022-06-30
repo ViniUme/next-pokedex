2db50a6acf488f97886259a505ab985450c68c9a
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-30 13:46:33 -0300

    Created navbar file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Navbar from '../components/navbar';
+
 export default function Index(){
     return(
-        <div>
-            
+        <div> 
+            <Navbar/>
         </div>
     )
 }
