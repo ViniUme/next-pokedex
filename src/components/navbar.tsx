5c92ecf585fdfd5433a035d538e9686a80b34d29
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-06-30 14:20:29 -0300

    Including logo in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/navbar.module.scss';
+
 export default function Navbar(){
     return(
-        <div>
-            
+        <div className={styles.navbar}>
+            <img src="/img/Pokemon-Logo.png" className={styles.logo} />
         </div>
     )
 }
