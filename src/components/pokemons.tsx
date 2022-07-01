37fb453599bad5fe3a56ab41a3e16faf4508704a
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-01 16:50:27 -0300

    Tested request pokemon

--- src/components/pokemons.tsx
+++ src/components/pokemons.tsx
@@ -1,4 +1,12 @@
-export default function Pokemons(){
+export default function Pokemons(): JSX.Element{
+
+    async function Request(): Promise<void>{
+        const respose: Response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        const json: any = await respose.json();
+        console.log(json)
+    }
+    Request();
+
     return(
         <div>
             
