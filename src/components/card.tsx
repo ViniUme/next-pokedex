a0888a90bb438764070bd819af7fbd18c1a421d2
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-04 16:24:03 -0300

    Editing cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,9 +3,21 @@ import Image from 'next/image';
 
 export default function Card({pokemon}: any): JSX.Element{
 
+    const get_id:string = `${pokemon.id / 100}`
+    const id:string = get_id.replace(".", "");
+
+    console.log(pokemon);
     return(
         <div className={styles.card}>
-            <Image src={`${pokemon.sprites.other["official-artwork"].front_default}`} width={150} height={150} alt="" />
+            <div className={styles.image_div}>
+                <Image src={`${pokemon.sprites.other["official-artwork"].front_default}`} width={150} height={150} alt="" />
+            </div>
+            <span className={styles.id}>#{id}</span>
+            <span className={styles.name}>{pokemon.species.name}</span>
+            
+            <div className={styles.types}>
+                
+            </div>
         </div>
     );
 }
