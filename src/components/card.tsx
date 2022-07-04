846dbcf090fce36a9c9284c2aaea627e256806d1
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-04 14:06:38 -0300

    Showing pokemon's image

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,11 +1,11 @@
 import styles from '../styles/card.module.scss';
+import Image from 'next/image';
 
 export default function Card({pokemon}: any): JSX.Element{
 
-    console.log(pokemon);
     return(
         <div className={styles.card}>
-
+            <Image src={`${pokemon.sprites.other["official-artwork"].front_default}`} width={150} height={150} alt="" />
         </div>
     );
 }
