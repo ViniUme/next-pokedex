c67ef67af22924f3097aa57bd5afc7795e2147d9
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-04 17:07:53 -0300

    Add and created Types

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,22 +1,22 @@
 import styles from '../styles/card.module.scss';
 import Image from 'next/image';
+import Types from './types'
 
 export default function Card({pokemon}: any): JSX.Element{
 
     const get_id:string = `${pokemon.id / 100}`
     const id:string = get_id.replace(".", "");
 
-    console.log(pokemon);
     return(
         <div className={styles.card}>
             <div className={styles.image_div}>
                 <Image src={`${pokemon.sprites.other["official-artwork"].front_default}`} width={150} height={150} alt="" />
             </div>
-            <span className={styles.id}>#{id}</span>
+            <span className={styles.id}>Nº{id}</span>
             <span className={styles.name}>{pokemon.species.name}</span>
             
             <div className={styles.types}>
-                
+                <Types types={pokemon.types} />
             </div>
         </div>
     );
---
+++ src/components/types.tsx
@@ -0,0 +1,32 @@
+import styles from '../styles/types.module.scss';
+
+export default function Types({types}: any): JSX.Element{
+
+    console.log(types[0].type.name);
+
+    if(types.length === 1){
+
+        const type = types[0].type.name
+
+        return(
+            <>
+                <span className={`${styles.type} ${type}`}>{type}</span>
+            </>
+        )
+    }
+    else{
+
+        const type = [
+            types[0].type.name,
+            types[1].type.name
+        ]
+
+        return(
+            <>
+                <span className={`${styles.type} ${type[0]}`}>{type[0]}</span>
+                <span className={`${styles.type} ${type[1]}`}>{type[1]}</span>
+            </>
+        )
+    }
+
+}
