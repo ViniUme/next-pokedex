4e74e9554f69f6addebd7542388f2a435b91025a
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-06 16:49:53 -0300

    Add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Navbar from '../components/navbar';
 import Pokemons from '../components/pokemons';
+import Head from 'next/head';
 
 export default function Index(){
     return(
         <div> 
+            <Head>
+                <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+            </Head>
             <Navbar/>
             <Pokemons/>
         </div>
