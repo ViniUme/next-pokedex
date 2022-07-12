1fa145c70c09c99618ed7072cf4814c041fc7cf8
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-12 13:39:59 -0300

    Add link in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from '../styles/navbar.module.scss';
+import Link from 'next/link';
 
 export default function Navbar(){
     return(
-        <div className={styles.navbar}>
-            <img src='/img/Pokemon-Logo.png' alt="" className={styles.soon} />
-        </div>
+        <Link href="/">
+            <a className={styles.navbar}>
+                <img src='/img/Pokemon-Logo.png' alt="" className={styles.soon} />
+            </a>
+        </Link>
     )
 }
