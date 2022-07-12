484c566096ae8172db2bc0e344a31171294fc174
Vinicius Henrique dos Santos <viniciusumetsu210@gmail.com>
2022-07-12 14:31:45 -0300

    Ajusting styles

--- src/pages/pokemon/[id].jsx
+++ src/pages/pokemon/[id].jsx
@@ -1,8 +1,9 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Types from '../../components/types';
 import Requests from '../../utils/requests-pokemons';
 import Navbar from '../../components/navbar';
-import { useEffect, useState } from 'react';
+import Head from 'next/head'
 import styles from '../../styles/id.module.scss';
 
 export default function IdPokemon(){
@@ -24,23 +25,29 @@ export default function IdPokemon(){
     if(pokemon != undefined){
         return(
             <>
+                <Head>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+                    <title>Pokemon {pokemon.name} Nº{pokemon.id}</title>
+                </Head>
+
                 <Navbar/>
+
                 <div className={`${pokemon.types[0].type.name} ${styles.container}`}>
                     <section className={styles.section}>
-                        <div className={styles.background_img}>
-                            <img className={styles.pokemon_img} alt='' src={`${pokemon.sprites.other["official-artwork"].front_default}`} />
-                        </div>
-
                         <div className={styles.main_info}>
 
                             <span>
-                                <h1 className={styles.name}>{pokemon.species.name}</h1>
+                                <h1 className={styles.name}>{pokemon.name} </h1>
                                 <h2 className={styles.id}>Nº{pokemon.id}</h2>
                             </span>
 
                             <span className={styles.types}>
                                 <Types types={pokemon.types} />
                             </span>
+                        </div>
+
+                        <div className={styles.background_img}>
+                            <img className={styles.pokemon_img} alt='' src={`${pokemon.sprites.other["official-artwork"].front_default}`} />
                         </div>
                     </section>
                 </div>
